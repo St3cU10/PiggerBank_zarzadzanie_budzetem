a65112b10659fd12a2de6fe55c07566043f7ecc9
JanKotynia <126583001+JanKotynia@users.noreply.github.com>
2023-04-24 21:52:46 +0200

    dodanie layoutu edycji kategorii

---
+++ app/src/main/java/com/example/piggerbank/EdycjaKategorii.kt
@@ -0,0 +1,11 @@
+package com.example.piggerbank
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EdycjaKategorii : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edycja_kategorii)
+    }
+}
