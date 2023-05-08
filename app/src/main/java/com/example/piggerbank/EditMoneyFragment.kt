d6c50f71e8384a824ccfe3d8e049cdf3595e424d
JanKotynia <126583001+JanKotynia@users.noreply.github.com>
2023-05-08 21:33:20 +0200

    Poprawienie layoutu edycji kategorii

--- app/src/main/java/com/example/piggerbank/EditMoneyFragment.kt
+++ app/src/main/java/com/example/piggerbank/EditMoneyFragment.kt
@@ -65,23 +65,23 @@ class EditMoneyFragment(val moneyID : Int) : Fragment() {
         val buttonDate : Button = view.findViewById(R.id.buttonDate)
 
 
-        oldName = view.findViewById(R.id.oldName)
-        oldCategory = view.findViewById(R.id.oldCat)
-        oldValue = view.findViewById(R.id.oldValue)
-        oldDate = view.findViewById(R.id.oldDate)
+        //oldName = view.findViewById(R.id.oldName)
+        //oldCategory = view.findViewById(R.id.oldCat)
+       // oldValue = view.findViewById(R.id.oldValue)
+        //oldDate = view.findViewById(R.id.oldDate)
 
-        oldName.text = moneyDB.moneyDao().getOneMoneyDescription(moneyID)
-        oldCategory.text = moneyDB.moneyDao().getOneMoneyCategory(moneyID)
-        oldValue.text = moneyDB.moneyDao().getOneMoneyValue(moneyID).toString()
-        oldDate.text = moneyDB.moneyDao().getOneMoneyDate(moneyID)
+       //oldName = moneyDB.moneyDao().getOneMoneyDescription(moneyID)
+       //oldCategory = moneyDB.moneyDao().getOneMoneyCategory(moneyID)
+        //oldValue = moneyDB.moneyDao().getOneMoneyValue(moneyID).toString()
+        //oldDate = moneyDB.moneyDao().getOneMoneyDate(moneyID)
 
         newNameEditText = view.findViewById(R.id.newName)
         newValueEditText = view.findViewById(R.id.newValue)
         newDataTextView = view.findViewById(R.id.newDate)
-        newDataTextView.text = oldDate.text.toString()
+        //newDataTextView.text = oldDate.text.toString()
 
-        newNameEditText.setText(oldName.text)
-        newValueEditText.setText(oldValue.text)
+        newNameEditText.setText(moneyDB.moneyDao().getOneMoneyDescription(moneyID))
+        newValueEditText.setText(moneyDB.moneyDao().getOneMoneyValue(moneyID).toString())
 
 
         // LISTA ROZSUWANA
@@ -94,7 +94,7 @@ class EditMoneyFragment(val moneyID : Int) : Fragment() {
         }
         newCatDropMenu = autoComplete
 
-        newCatDropMenu.setText(oldCategory.text.toString())
+        newCatDropMenu.setText(moneyDB.moneyDao().getOneMoneyCategory(moneyID))
         // KALENDARZ
         val calendarBox = Calendar.getInstance()
         val dateBox = DatePickerDialog.OnDateSetListener { datePicker, year, month, day ->
