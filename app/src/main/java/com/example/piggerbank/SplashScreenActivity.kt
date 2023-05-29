2594e61f795be6b480390e22731eb4b75a324e49
St3cU10 <arturstelcer10@gmail.com>
2023-05-30 00:45:53 +0200

    poprawki splash screena oraz fragmentu skarbonki i edycja nagłówka menu

--- app/src/main/java/com/example/piggerbank/SplashScreenActivity.kt
+++ app/src/main/java/com/example/piggerbank/SplashScreenActivity.kt
@@ -12,8 +12,19 @@ class SplashScreenActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash_screen)
 
         val logo : ImageView = findViewById(R.id.logo)
+        val napis : ImageView = findViewById(R.id.napis)
+
+
         logo.alpha=0f
         logo.animate().setDuration(2000).alpha(1f).withEndAction{
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        }
+
+        napis.alpha=0f
+        napis.animate().setDuration(2000).alpha(1f).withEndAction{
             val i = Intent(this, MainActivity::class.java)
             startActivity(i)
             overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
