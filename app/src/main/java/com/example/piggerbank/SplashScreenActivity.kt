6f033a4ec54ed4ff3e4a6952425937d940677e2d
St3cU10 <arturstelcer10@gmail.com>
2023-05-30 00:01:35 +0200

    dodanie Splash screena

---
+++ app/src/main/java/com/example/piggerbank/SplashScreenActivity.kt
@@ -0,0 +1,25 @@
+package com.example.piggerbank
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.core.graphics.alpha
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val logo : ImageView = findViewById(R.id.logo)
+        logo.alpha=0f
+        logo.animate().setDuration(2000).alpha(1f).withEndAction{
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        }
+
+
+    }
+}
